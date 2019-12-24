6bae977879239acefd23d712d4f2e3e53e9ff192
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-24 07:34:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,6 +47,7 @@ def monopoly_with_fourth_tier
 		:railroads =>
 		{
 			:pieces => 4,
+
 			:rent_in_dollars =>
 				{
 					:one_piece_owned => 25,
@@ -54,12 +55,17 @@ def monopoly_with_fourth_tier
 					:three_pieces_owned => 100,
 					:four_pieces_owned => 200
 				},
+				
 			:names =>
 				{
-					:reading_railroad => 100,
-					:pennsylvania_railroad => 200,
-					:b_and_o_railroad => 400,
-					:shortline_railroad => 800
+					:reading_railroad => 
+						{"mortgage_value" = "100"},
+					:pennsylvania_railroad => 
+						{"mortgage_value" = "200"},
+					:b_and_o_railroad =>
+						{"mortgage_value" = "400"},
+					:shortline_railroad =>
+						{"mortgage_value" = "800"}
 				}
 		}
 	}
